9e6b8b934bdff04998ca802a8252bea2e0c93211
Martin Karlsson <m.karlsson91@gmail.com>
2025-09-30 22:42:58 +0200

    Added case handling for zone and date commands

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,20 +3,49 @@ package com.example;
 import com.example.api.ElpriserAPI;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import com.example.api.ElpriserAPI.Prisklass;
 
 public class Main {
     public static void main(String[] args) {
-        for( var arg : args )
+        ElpriserAPI api = new ElpriserAPI();
+        List<String> listOfArgs = Arrays.asList(args);
+        Prisklass pricingZone = Prisklass.SE1;
+        LocalDate date;
+
+        if (listOfArgs.contains("--zone")) {
+            String zone = listOfArgs.get(listOfArgs.indexOf("--zone") + 1);
+            try {
+                pricingZone = Prisklass.valueOf(zone.toUpperCase());
+            } catch (IllegalArgumentException e){
+                System.out.println("--zone must be followed by one of the specific zone values:" +
+                        " SE1, SE2, SE3, SE4");
+            }
+        } else {
+            throw new IllegalArgumentException("It is required to provide a --zone argument, " +
+                    "provide argument --help for more information");
+        }
+        if (listOfArgs.contains("--date")) {
+            date = LocalDate.parse(listOfArgs.get(listOfArgs.indexOf("--date") + 1));
+        } else {
+            date = LocalDate.now();
+        }
+
+        List<ElpriserAPI.Elpris> priceList = api.getPriser(date, pricingZone);
+
+
+
+
+        for (var arg : args)
             System.out.println(arg);
-        ElpriserAPI elpriserAPI = new ElpriserAPI();
-        ElpriserAPI.Prisklass prisklass = ElpriserAPI.Prisklass.valueOf(args[1]);
+
         LocalDate datum = LocalDate.parse(args[3]);
-        List<ElpriserAPI.Elpris> dagensPriser = elpriserAPI.getPriser(datum, prisklass);
-//        IO.println(dagensPriser.toString());
+        //        IO.println(dagensPriser.toString());
         double prisSumma = 0.0;
-        int antalTimIntervall = dagensPriser.toArray().length;
-        for (var elpris : dagensPriser) {
+        int antalTimIntervall = priceList.toArray().length;
+        for (var elpris : priceList) {
             double timpris = elpris.sekPerKWh();
             prisSumma += timpris;
             System.out.printf("""
@@ -26,7 +55,6 @@ public class Main {
         }
         double medelPris = prisSumma / antalTimIntervall;
         System.out.println("Medelpris: " + medelPris + " kr");
-
     }
 
 }
